fb7168005879910c213a621878ecc437da0672ba
threeice <threeice@outlook.com>
2026-02-05 21:28:50 +0800

    refactor: move the logic to struct from extension function

--- Runtime/Core/EventExtensions.cs
+++ Runtime/Core/EventExtensions.cs
@@ -1,7 +1,5 @@
 using Unity.Entities;
 using Unity.Collections;
-using Unity.Jobs;
-using Unity.Jobs.LowLevel.Unsafe;
 
 namespace IceEvents
 {
@@ -9,143 +7,25 @@ namespace IceEvents
     {
         public static EventWriter<T> GetWriter<T>(this RefRW<EventBuffer<T>> buffer) where T : unmanaged, IEvent
         {
-            return new EventWriter<T>
-            {
-                BufferUpdate = buffer.ValueRW.BufferUpdateCurrent,
-                BufferFixed = buffer.ValueRW.BufferFixedCurrent
-            };
+            return buffer.ValueRW.GetWriter();
         }
 
-        public static EventWriter<T> GetWriter<T>(this in EventBuffer<T> buffer) where T : unmanaged, IEvent
-        {
-            return new EventWriter<T>
-            {
-                BufferUpdate = buffer.BufferUpdateCurrent,
-                BufferFixed = buffer.BufferFixedCurrent
-            };
-        }
-
-        public static EventReader<T> GetUpdateReader<T>(this in EventBuffer<T> buffer, ulong startId, Allocator allocator)
+        public static EventReader<T> GetUpdateReader<T>(this RefRW<EventBuffer<T>> buffer, ulong startId, Allocator allocator)
             where T : unmanaged, IEvent
-            => new EventReader<T>(startId, EventLoopType.Update, allocator);
+            => buffer.ValueRO.GetUpdateReader(startId, allocator);
 
-        public static EventReader<T> GetFixedReader<T>(this in EventBuffer<T> buffer, ulong startId, Allocator allocator)
+        public static EventReader<T> GetFixedReader<T>(this RefRW<EventBuffer<T>> buffer, ulong startId, Allocator allocator)
             where T : unmanaged, IEvent
-            => new EventReader<T>(startId, EventLoopType.FixedUpdate, allocator);
+            => buffer.ValueRO.GetFixedReader(startId, allocator);
 
-        public static EventReader<T> GetUpdateReader<T>(this in EventBuffer<T> buffer, Allocator allocator)
+        public static EventReader<T> GetUpdateReader<T>(this RefRW<EventBuffer<T>> buffer, Allocator allocator)
             where T : unmanaged, IEvent
-            => new EventReader<T>(0, EventLoopType.Update, allocator);
+            => buffer.ValueRO.GetUpdateReader(allocator);
 
-        public static EventReader<T> GetFixedReader<T>(this in EventBuffer<T> buffer, Allocator allocator)
+        public static EventReader<T> GetFixedReader<T>(this RefRW<EventBuffer<T>> buffer, Allocator allocator)
             where T : unmanaged, IEvent
-            => new EventReader<T>(0, EventLoopType.FixedUpdate, allocator);
-    }
+            => buffer.ValueRO.GetFixedReader(allocator);
 
-    public struct EventReader<T> : System.IDisposable where T : unmanaged, IEvent
-    {
-        private NativeArray<ulong> _bookmark;
-        private readonly EventLoopType _loopType;
-
-        private NativeList<T> _prev;
-        private NativeList<T> _curr;
-        private ulong _baseIdPrev;
-
-        public EventReader(ulong startId, EventLoopType loopType, Allocator allocator)
-        {
-            _bookmark = new NativeArray<ulong>(1, allocator);
-            _bookmark[0] = startId;
-            _loopType = loopType;
-            _prev = default;
-            _curr = default;
-            _baseIdPrev = 0;
-        }
-
-        public void Update(in EventBuffer<T> buffer)
-        {
-            if (_loopType == EventLoopType.Update)
-            {
-                _prev = buffer.BufferUpdatePrevious;
-                _curr = buffer.BufferUpdateCurrent;
-                _baseIdPrev = buffer.BaseIdUpdatePrev;
-            }
-            else
-            {
-                _prev = buffer.BufferFixedPrevious;
-                _curr = buffer.BufferFixedCurrent;
-                _baseIdPrev = buffer.BaseIdFixedPrev;
-            }
-        }
-
-        public Enumerator GetEnumerator()
-        {
-            return new Enumerator(_prev, _curr, _bookmark, _baseIdPrev);
-        }
-
-        public void Dispose()
-        {
-            if (_bookmark.IsCreated) _bookmark.Dispose();
-        }
-
-        public struct Enumerator
-        {
-            private readonly NativeList<T> _prev;
-            private readonly NativeList<T> _curr;
-            private NativeArray<ulong> _bookmark;
-            private readonly ulong _baseIdPrev;
-            private readonly ulong _baseIdCurr;
-
-            private int _indexPrev;
-            private int _indexCurr;
-            private T _currentElement;
-
-            public Enumerator(NativeList<T> prev, NativeList<T> curr, NativeArray<ulong> bookmark, ulong baseIdPrev)
-            {
-                _prev = prev;
-                _curr = curr;
-                _bookmark = bookmark;
-                _baseIdPrev = baseIdPrev;
-                _baseIdCurr = baseIdPrev + (ulong)prev.Length;
-                _currentElement = default;
-
-                ulong targetId = bookmark[0] + 1;
-
-                long relIndexPrev = (long)(targetId - _baseIdPrev) - 1;
-                if (relIndexPrev < 0) relIndexPrev = 0;
-                _indexPrev = (int)relIndexPrev - 1;
-
-                long relIndexCurr = (long)(targetId - _baseIdCurr) - 1;
-                if (relIndexCurr < 0) relIndexCurr = 0;
-                _indexCurr = (int)relIndexCurr - 1;
-            }
-
-            public T Current => _currentElement;
-
-            public bool MoveNext()
-            {
-                if (_indexPrev < _prev.Length - 1)
-                {
-                    _indexPrev++;
-                    _currentElement = _prev[_indexPrev];
-                    _bookmark[0] = _baseIdPrev + (ulong)_indexPrev + 1;
-                    return true;
-                }
-
-                if (_indexCurr < _curr.Length - 1)
-                {
-                    _indexCurr++;
-                    _currentElement = _curr[_indexCurr];
-                    _bookmark[0] = _baseIdCurr + (ulong)_indexCurr + 1;
-                    return true;
-                }
-
-                return false;
-            }
-        }
-    }
-
-    public static class EventBufferParallelExtensions
-    {
         /// <summary>
         /// Creates a parallel event writer handle with specified batch count.
         /// The batch count must match the number of batches that will write to the stream.
@@ -156,18 +36,7 @@ namespace IceEvents
             int batchCount,
             Allocator allocator) where T : unmanaged, IEvent
         {
-            if (batchCount <= 0)
-                throw new System.ArgumentException("Batch count must be greater than 0", nameof(batchCount));
-
-            var stream = new NativeStream(batchCount, allocator);
-            return new ParallelEventWriterHandle<T>
-            {
-                Writer = new ParallelEventWriter<T>
-                {
-                    Writer = stream.AsWriter()
-                },
-                Stream = stream
-            };
+            return buffer.ValueRW.GetParallelWriter(batchCount, allocator);
         }
     }
 }
--- Runtime/Core/EventInfrastructure.cs
+++ Runtime/Core/EventInfrastructure.cs
@@ -29,6 +29,48 @@ namespace IceEvents
         public ulong BaseIdFixedPrev;
 
         public bool IsCreated => BufferUpdateCurrent.IsCreated;
+
+        public EventWriter<T> GetWriter()
+        {
+            return new EventWriter<T>
+            {
+                BufferUpdate = BufferUpdateCurrent,
+                BufferFixed = BufferFixedCurrent
+            };
+        }
+
+        public readonly EventReader<T> GetUpdateReader(ulong startId, Allocator allocator)
+            => new(startId, EventLoopType.Update, allocator);
+
+        public readonly EventReader<T> GetFixedReader(ulong startId, Allocator allocator)
+            => new(startId, EventLoopType.FixedUpdate, allocator);
+
+        public readonly EventReader<T> GetUpdateReader(Allocator allocator)
+            => new(0, EventLoopType.Update, allocator);
+
+        public readonly EventReader<T> GetFixedReader(Allocator allocator)
+            => new(0, EventLoopType.FixedUpdate, allocator);
+
+        /// <summary>
+        /// Creates a parallel event writer handle with specified batch count.
+        /// The batch count must match the number of batches that will write to the stream.
+        /// For IJobFor, this should match the batch count used in Schedule().
+        /// </summary>
+        public ParallelEventWriterHandle<T> GetParallelWriter(int batchCount, Allocator allocator)
+        {
+            if (batchCount <= 0)
+                throw new System.ArgumentException("Batch count must be greater than 0", nameof(batchCount));
+
+            var stream = new NativeStream(batchCount, allocator);
+            return new ParallelEventWriterHandle<T>
+            {
+                Writer = new ParallelEventWriter<T>
+                {
+                    Writer = stream.AsWriter()
+                },
+                Stream = stream
+            };
+        }
     }
 
     public struct EventWriter<T> where T : unmanaged, IEvent
@@ -112,4 +154,106 @@ namespace IceEvents
     }
 
     public enum EventLoopType { Update, FixedUpdate }
+
+    public struct EventReader<T> : System.IDisposable where T : unmanaged, IEvent
+    {
+        private NativeArray<ulong> _bookmark;
+        private readonly EventLoopType _loopType;
+
+        private NativeList<T> _prev;
+        private NativeList<T> _curr;
+        private ulong _baseIdPrev;
+
+        public EventReader(ulong startId, EventLoopType loopType, Allocator allocator)
+        {
+            _bookmark = new NativeArray<ulong>(1, allocator);
+            _bookmark[0] = startId;
+            _loopType = loopType;
+            _prev = default;
+            _curr = default;
+            _baseIdPrev = 0;
+        }
+
+        public void Update(in EventBuffer<T> buffer)
+        {
+            if (_loopType == EventLoopType.Update)
+            {
+                _prev = buffer.BufferUpdatePrevious;
+                _curr = buffer.BufferUpdateCurrent;
+                _baseIdPrev = buffer.BaseIdUpdatePrev;
+            }
+            else
+            {
+                _prev = buffer.BufferFixedPrevious;
+                _curr = buffer.BufferFixedCurrent;
+                _baseIdPrev = buffer.BaseIdFixedPrev;
+            }
+        }
+
+        public Enumerator GetEnumerator()
+        {
+            return new Enumerator(_prev, _curr, _bookmark, _baseIdPrev);
+        }
+
+        public void Dispose()
+        {
+            if (_bookmark.IsCreated) _bookmark.Dispose();
+        }
+
+        public struct Enumerator
+        {
+            private readonly NativeList<T> _prev;
+            private readonly NativeList<T> _curr;
+            private NativeArray<ulong> _bookmark;
+            private readonly ulong _baseIdPrev;
+            private readonly ulong _baseIdCurr;
+
+            private int _indexPrev;
+            private int _indexCurr;
+            private T _currentElement;
+
+            public Enumerator(NativeList<T> prev, NativeList<T> curr, NativeArray<ulong> bookmark, ulong baseIdPrev)
+            {
+                _prev = prev;
+                _curr = curr;
+                _bookmark = bookmark;
+                _baseIdPrev = baseIdPrev;
+                _baseIdCurr = baseIdPrev + (ulong)prev.Length;
+                _currentElement = default;
+
+                ulong targetId = bookmark[0] + 1;
+
+                long relIndexPrev = (long)(targetId - _baseIdPrev) - 1;
+                if (relIndexPrev < 0) relIndexPrev = 0;
+                _indexPrev = (int)relIndexPrev - 1;
+
+                long relIndexCurr = (long)(targetId - _baseIdCurr) - 1;
+                if (relIndexCurr < 0) relIndexCurr = 0;
+                _indexCurr = (int)relIndexCurr - 1;
+            }
+
+            public T Current => _currentElement;
+
+            public bool MoveNext()
+            {
+                if (_indexPrev < _prev.Length - 1)
+                {
+                    _indexPrev++;
+                    _currentElement = _prev[_indexPrev];
+                    _bookmark[0] = _baseIdPrev + (ulong)_indexPrev + 1;
+                    return true;
+                }
+
+                if (_indexCurr < _curr.Length - 1)
+                {
+                    _indexCurr++;
+                    _currentElement = _curr[_indexCurr];
+                    _bookmark[0] = _baseIdCurr + (ulong)_indexCurr + 1;
+                    return true;
+                }
+
+                return false;
+            }
+        }
+    }
 }
